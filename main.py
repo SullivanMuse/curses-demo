ed429a35ae32471471c4e5da3b7bcc7b9705fbfd
Sullivan Muse <sullivan.muse@gmail.com>
2023-10-05 23:25:49 -0500

    Implement table column solver

--- main.py
+++ main.py
@@ -2,112 +2,121 @@
 
 import curses
 import curses.textpad
-import argparse
-import fnmatch
+from math import floor
 
 def clamp(x, a, b):
     return min(max(x, a), b)
 
 NORMAL, EDIT = range(2)
 
+bl = "└"
+br = "┘"
+tl = "┌"
+tr = "┐"
+
+tm = "┬"
+lm = "├"
+rm = "┤"
+bm = "┴"
+
+h = "─"
+v = "│"
+
+c = "┼"
+
+class Table:
+    def __init__(self, win):
+        super().__init__()
+        self.win = win
+        self.header_rows = 0
+        self.items = []
+
+        # Constraints to solve for each column
+        #   Unconstrained columns are allocated equal proportion of the leftover space
+        #   Constraints on non-existent columns are ignored
+        self.constraints = {}
+
+        # Solution to constraint problem
+        #   If there is no solution, the entire table is set to pink
+        self.solution = {}
+
+    def set_weight(self, column, weight):
+        self.constraints[column] = {"type": "weight", "value": weight}
+
+    def set_width(self, column, width):
+        self.constraints[column] = {"type": "width", "value": width}
+
+    def solve(self):
+        n = max(len(row) for row in self.items)
+        solution = [None for _ in range(n)]
+        (_, w) = self.win.getyx()
+
+        # Horizontal space not taken by frames
+        leftover = w - n - 1
+
+        # Allocate widths
+        for i in range(n):
+            w = self.constraints.get(i)
+            if w is not None:
+                if w["type"] == "width":
+                    solution[i] = w["value"]
+                    leftover -= solution[i]
+
+        # Allocate weights
+        space = leftover
+        for i in range(n):
+            w = self.constraints.get(i)
+            if w is not None and w["type"] == "weight":
+                solution[i] = floor(space * w["value"])
+                leftover -= solution[i]
+
+        # Allocated unconstrained weights
+        unconstrained_col_width = leftover // solution.count(None)
+        for i in range(n):
+            w = self.constraints.get(i)
+            if w is None:
+                solution[i] = unconstrained_col_width
+                leftover -= solution[i]
+
+        # Donate any leftover to the first weighted column
+        for i in range(n):
+            w = self.constraints.get(i)
+            if w is None or w["type"] == "weight":
+                solution[i] += leftover
+                break
+
+        # Not enought space for allocation
+        if leftover < 0:
+            raise ValueError("Solver failed")
+
+        return solution
+
+    def scroll_down(self):
+        pass
+
+    def scroll_up(self):
+        pass
+
+    def scroll_top(self):
+        pass
+
+    def scroll_bottom(self):
+        pass
+
+    def draw(self):
+        (h, w) = self.win.getyx()
+
 def main(stdscr):
-    global key
+    scr1 = curses.newwin(14, 14)
+
     stdscr.clear()
+    stdscr.border()
     stdscr.refresh()
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input")
-    args = parser.parse_args()
-    with open(args.input, "r") as file:
-        lines = list(line.strip() for line in file)
+    scr1.clear()
+    scr1.border()
+    scr1.refresh()
 
-    # Prevent curses from blocking on stdscr.getch()
-    stdscr.nodelay(True)
-
-    def newline():
-        (y, _) = stdscr.getyx()
-        stdscr.move(y + 1, 0)
-
-    debug = False
-    pattern = ""
-    r = 0
-    s = 0
-    mode = "normal"
-    while True:
-        (h, w) = stdscr.getmaxyx()
-        key = stdscr.getch()
-
-        # Handle events
-
-        # Normal mode
-        if mode == "normal":
-            if key == ord("q"):
-                break
-            elif key == ord("d"):
-                debug = not debug
-            elif key == ord("/"):
-                mode = "edit"
-                continue
-
-        # Edit filter mode
-        elif mode == "edit":
-            if key == ord("\n"):
-                mode = "normal"
-                continue
-
-            # Insert character
-            elif ord("a") <= key <= ord("z") or ord("A") <= key <= ord("Z") or key == ord(" ") or key == ord("\t"):
-                pattern = pattern[:r] + chr(key) + pattern[r:]
-                r += 1
-            elif key == curses.KEY_LEFT:
-                r -= 1
-            elif key == curses.KEY_RIGHT:
-                r += 1
-            elif key == curses.KEY_BACKSPACE:
-                # Delete character at r-1
-                pattern = pattern[:r-1] + pattern[r:]
-                r -= 1
-            elif key == curses.KEY_HOME:
-                r = 0
-            elif key == curses.KEY_END:
-                r = len(pattern)
-            elif key == curses.KEY_DC:
-                # Delete character at r
-                pattern = pattern[:r] + pattern[r+1:]
-
-            # Clamp position
-            r = clamp(r, 0, len(pattern))
-
-            # Adjust scroll position
-            window_max = s + w - 1
-            if r > window_max:
-                s += r - window_max
-            if r < s:
-                s = r
-
-        # Draw
-        current = fnmatch.filter(lines, f"*{pattern}*")
-        stdscr.clear()
-        stdscr.move(0, 0)
-        for line in current:
-            i = line.index(pattern)
-            stdscr.addstr(line[:i])
-            stdscr.addstr(line[i:i+len(pattern)], curses.A_BOLD)
-            stdscr.addstr(line[i+len(pattern):])
-            newline()
-        if debug:
-            stdscr.move(0, 0)
-            stdscr.addnstr(f"(h, w) = {(h, w)}", w - 1); newline()
-            stdscr.addnstr(f"(r, s) = {(r, s)}", w - 1); newline()
-            stdscr.addnstr(f"mode = {mode}", w - 1); newline()
-            stdscr.addnstr(f"buffer = {repr(pattern)}", w - 1); newline()
-        if mode == "edit":
-            stdscr.move(h - 1, 0)
-            stdscr.addnstr(pattern[s : s + w - 1], w - 1)
-            stdscr.move(h-1, r - s)
-        stdscr.refresh()
-
-        # Limit the FPS
-        curses.napms(16)
+    stdscr.getch()
 
 curses.wrapper(main)
